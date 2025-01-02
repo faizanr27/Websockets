83860763e1d70689d2d7fdd6cf3f5d2f1bbdf8df
Faizan Raza <faizanms025347@gmail.com>
2025-01-02 23:51:50 +0530

    ping pong

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080})
+
+//event handler
+wss.on("connection", function(socket)
+{
+    socket.on("message", (e)=>{
+        if (e.toString() === "ping") {
+            socket.send("pong")
+        }
+    })
+}
+)
